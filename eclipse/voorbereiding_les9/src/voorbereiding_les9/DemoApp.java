8cc1cfd8486718abc8f93a8a2f15524a1e2ae8f3
Cathelijne de Bruijn <cathelijne017@gmail.com>
2018-03-02 13:12:46 +0100

    voorbereiding les9

---
+++ eclipse/voorbereiding_les9/src/voorbereiding_les9/DemoApp.java
@@ -0,0 +1,9 @@
+package voorbereiding_les9;
+
+
+public class DemoApp {
+	public static void main(String[] args) {
+		Student s = new Student("han");
+		System.out.println(s.getNaam());
+	}
+}
---
+++ eclipse/voorbereiding_les9/src/voorbereiding_les9/Student.java
@@ -0,0 +1,52 @@
+/////Opgave Student uitbreiden
+
+package voorbereiding_les9;
+
+public class Student {
+	private String naam;
+	private String geslacht;
+	private int nummer;
+	
+	public static final String MAN = "man";
+	public static final String VROUW = "vrouw";
+	
+	private static int nStudenten = 0;
+	
+	public Student(String naam) {
+		this.naam = naam;
+		nStudenten++;
+	}
+		
+	public String getGeslacht() {
+		return geslacht;
+	}
+	
+	public void setGeslacht(String geslacht) {
+		this.geslacht = geslacht;
+	}
+	
+	public static int getNStudenten() {
+		return nStudenten;
+	}
+	
+	public String toString() {
+		return getNaam();
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+	public void setNaam(String naam) {
+		this.naam = naam;
+}
+
+	public int getNummer() {
+		return nummer;
+	}
+
+	public void setNummer(int nummer) {
+		this.nummer = nummer;
+	}
+}
+
